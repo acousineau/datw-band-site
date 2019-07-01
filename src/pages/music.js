5605e286af3bdca8d3521be6890f9e8c517bd064
Andy Cousineau <andrew.cousineau@gmail.com>
2019-07-01 16:31:58 -0400

    Ensure music page is scrollable

--- src/pages/music.js
+++ src/pages/music.js
@@ -7,7 +7,7 @@ import {
 } from '@fortawesome/free-brands-svg-icons'
 
 import Layout from '../components/layout'
-import Background from '../images/bg-music.jpg'
+import Background from '../images/bg-music.png'
 
 import './Music.scss'
 
@@ -19,19 +19,20 @@ const Music = () => (
         style={{
           backgroundImage: `url(${Background})`,
         }}
-      ></div>
-      <div className="music-container">
-        <h1>Stream or Download Below:</h1>
-        <div className="links-container">
-          <a href="https://open.spotify.com/artist/3a5h90HoFJJTe6FwnF4zIx?si=0tPa5KrOTVOUEv0rZZWOaw">
-            <FontAwesomeIcon icon={faSpotify} /> Spotify
-          </a>
-          <a href="https://music.apple.com/us/album/the-rivers-gonna-rise-pt-3-single/1440085688">
-            <FontAwesomeIcon icon={faItunes} /> Apple Music
-          </a>
-          <a href="https://dropbox.com" style={{ marginBottom: '0' }}>
-            <FontAwesomeIcon icon={faDropbox} /> Dropbox
-          </a>
+      >
+        <div className="music-container">
+          <h1>Stream or Download Below:</h1>
+          <div className="links-container">
+            <a href="https://open.spotify.com/artist/3a5h90HoFJJTe6FwnF4zIx?si=0tPa5KrOTVOUEv0rZZWOaw">
+              <FontAwesomeIcon icon={faSpotify} /> Spotify
+            </a>
+            <a href="https://music.apple.com/us/album/the-rivers-gonna-rise-pt-3-single/1440085688">
+              <FontAwesomeIcon icon={faItunes} /> Apple Music
+            </a>
+            <a href="https://dropbox.com" style={{ marginBottom: '0' }}>
+              <FontAwesomeIcon icon={faDropbox} /> Dropbox
+            </a>
+          </div>
         </div>
       </div>
     </section>
