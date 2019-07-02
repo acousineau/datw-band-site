518f92684646faf87bb566b8ab8c9538f8fe5546
Andy Cousineau <andrew.cousineau@gmail.com>
2019-07-02 12:45:56 -0400

    404 styling

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,10 +13,10 @@ module.exports = {
         trackingId: 'UA-75666159-2',
         // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: true,
-        // // Setting this parameter is optional
-        // anonymize: true,
-        // // Setting this parameter is also optional
-        // respectDNT: true,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
         // // Avoids sending pageview hits from custom paths
         // exclude: ["/preview/**", "/do-not-track/me/too/"],
         // // Enables Google Optimize using your container Id
--- src/pages/404.js
+++ src/pages/404.js
@@ -10,8 +10,10 @@ const NotFoundPage = () => (
     <SEO title="404: Not found" />
     <section id="four-oh-four">
       <div className="container">
-        <h1>NOT FOUND</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        <div className="contents">
+          <h1>NOT FOUND</h1>
+          <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        </div>
       </div>
     </section>
   </Layout>
